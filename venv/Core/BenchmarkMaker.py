46a87b8f9cfa0e31873b46987af7ebef20db46ee
chrismatthewcyril <cyril_chrismatthew@outlook.com>
2021-03-04 10:24:09 -0800

    Finished commenting

--- venv/Core/BenchmarkMaker.py
+++ venv/Core/BenchmarkMaker.py
@@ -14,12 +14,13 @@ def run_program(repository):
     # Step 1 We need sequences! To save time, we'll keep the first sequence in each file. What are the files?
     human_files, fly_files = sc.get_filenames(repository)
 
-    # Step 2 construct the full filepaths, and get the FIRST sequences only and store them in a dictionary. Key = filename,
-    # Value = String seq.
+    # Step 2 construct the full filepaths, and get the FIRST sequences only and store them in a dictionary.
+    # Key = filename, Value = String seq.
 
     human_seqs_dict = {}
     fly_seqs_dict = {}
 
+    # Getting seqs from the DataBank.
     for human_file, fly_file in zip(human_files, fly_files):
         human_seqs_dict[human_file] = sc.get_first_seq(repository + human_file)
         fly_seqs_dict[fly_file] = sc.get_first_seq(repository + fly_file)
@@ -39,7 +40,7 @@ def run_program(repository):
         # The first statement below will give me data for a histogram.
         # The second statement computes the average length of the two seqs and passes it as well as the score
         rec.add_pair_score(human_key, fly_key, needle_scores[human_key + " vs " + fly_key])
-        rec.add_length_data(len(human_seqs_dict[human_key])+len(fly_seqs_dict[fly_key])/2,
+        rec.add_length_data(len(human_seqs_dict[human_key]) + len(fly_seqs_dict[fly_key]) / 2,
                             needle_scores[human_key + " vs " + fly_key])
 
     # Now we need to compute the average normalized scores for each pairing
@@ -53,16 +54,20 @@ def run_program(repository):
     print("\nBenchmark Average Alignment Score: " + str(benchmark_average) + " points per base.")
 
 
+# Helper method, returns average
 def get_benchmark_average():
     return benchmark_average
 
 
+# Helper method, sets the recorder's gene deviation score.
 def record_benchmark_average():
     rec.set_gene_dev_score(benchmark_average)
 
 
+# Helper method, returns the reference to the local recorder object.
 def get_recorder_object():
     return rec
+
 
 class Benchmarker:
     pass
